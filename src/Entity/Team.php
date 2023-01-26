54b08c64529b95bfc34942c43446bc9799d1aadb
unknown <christelle.kandel@gmail.com>
2023-01-26 14:55:26 +0100

    creation d'une table TEAM dans la BDD

---
+++ src/Entity/Team.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $Niveau = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $Name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNiveau(): ?int
+    {
+        return $this->Niveau;
+    }
+
+    public function setNiveau(int $Niveau): self
+    {
+        $this->Niveau = $Niveau;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(?string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+}
