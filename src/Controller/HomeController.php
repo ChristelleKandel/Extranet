a134e61d66271ff2a8fa668bcca3589eba7acc2f
unknown <christelle.kandel@gmail.com>
2023-03-14 15:56:50 +0100

    choix de la page d'accueil admin ou user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,19 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $user = $this->getUser();
+        $role = $user->getRoles();
+        // dd(in_array('ROLE_ADMIN', $role));
+        if (in_array('ROLE_ADMIN', $role)){
+            return $this->render('home/index.html.twig');
+        }else{
+            return $this->render('account/show_account.html.twig', [
+                'user' => $user,
+            ]);
+        }
+        // return $this->render('home/index.html.twig');
     }
+
     #[Route('/home/contact', name: 'app_home_contact')]
     public function contact(): Response
     {
